eec028aa40b7380c62457a85c94b726336d9d29e
maricotab <161454065+maricotab@users.noreply.github.com>
2025-01-17 10:27:17 -0300

    Update Prato.ts

--- src/Prato.ts
+++ src/Prato.ts
@@ -1,17 +1,50 @@
 export class Prato{
-    private quantidade:number
-    private nome:string
-    private preco:number
-    private descricao:string
+    private _quantidade:number
+    private _nome:string
+    private _preco:number
+    private _descricao:string
 
-    constructor(nome: string, preco: number,descricao:string) {
-        this.nome = nome;
-        this.preco = preco;
-        this.descricao=descricao
+    constructor(quantidade: number, nome: string, preco: number, descricao:string) {
+        this._quantidade = quantidade 
+        this._nome = nome;
+        this._preco = preco;
+        this._descricao=descricao
+    }
+
+    get quantidade(): number {
+        return this._quantidade;
+    }
+
+    set quantidade(quantidade: number) {
+        this._quantidade = quantidade;
+    }
+
+    get nome(): string {
+        return this._nome;
+    }
+
+    set nome(nome: string) {
+        this._nome = nome;
+    }
+
+    get preco(): number {
+        return this._preco;
+    }
+
+    set preco(preco: number) {
+        this._preco = preco;
+    }
+
+    get descricao(): string {
+        return this._descricao;
+    }
+
+    set descricao(descricao: string) {
+        this._descricao = descricao
     }
 
     adicionar_quantidade(n:number){
-        this.quantidade= n
+        this._quantidade= n
         return;
     }
 }
